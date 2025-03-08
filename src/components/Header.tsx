1030a920e2b22d95a3ec0a00d5834b23921cfbb7
wesalshehri <113001924+wesalshehri@users.noreply.github.com>
2025-03-07 23:56:56 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,25 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+    background-color: #34495e;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 50px; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000; 
+`;
 
 export default function Header() {
     return (
-        <header>
-            <h1>My React Resume</h1>
-            <p>A professional online resume built with React & TypeScript</p>
-        </header>
+        <StyledHeader>
+            <h1 style={{ fontSize: "20px", margin: 0 }}>My React Resume</h1>
+        </StyledHeader>
     );
 }
